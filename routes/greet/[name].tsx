a98e2a6773315be848b6f6c49b160075fff12f96
Danil Kazakov <taigabrew@gmail.com>
2022-08-19 15:23:28 +0600

    Init

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,16 @@
+/** @jsx h */
+import { h } from 'preact'
+import { PageProps } from '$fresh/server.ts'
+
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params
+
+  return (
+    <main>
+      <p>Greetings to you, {name}!</p>
+      {/* <code>
+        {JSON.stringify(props)}
+      </code> */}
+    </main>
+  )
+}
